aa8e8a76c6dfaf78a8f7da3754fe38cf4e02068d
Yuy-1 <DM-Yuy@proton.me>
2022-11-23 05:58:42 -0500

    refactor project inside virtual python environment create download_info function

--- program.py
+++
@@ -1,9 +0,0 @@
-
-
-def main():
-    pass
-
-def print_header():
-    print("----------------------------------")
-    print("        PODCAST DOWNLOADER        ")
-    PRINT("----------------------------------")
