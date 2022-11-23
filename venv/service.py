5687cbc43c588d475de1d796bf7252b2d7883d66
Yuy-1 <DM-Yuy@proton.me>
2022-11-23 06:31:03 -0500

    fix spelling error in service.py

---
+++ venv/service.py
@@ -0,0 +1,14 @@
+import requests
+
+def download_info():
+    url = 'https://talkpython.fm/episodes/rss'
+
+    resp = requests.get(url)
+    resp.raise_for_status()
+
+    test = resp.text
+    print(test)
+
+    return None
+
+#please notice my changes git
