740854f8630725476815d73da9a993dd85a9d185
Yuy-1 <DM-Yuy@proton.me>
2022-11-25 09:10:24 -0500

    add requirements.txt

--- venv/service.py
+++ venv/service.py
@@ -1,4 +1,11 @@
+from collections import namedtuple
+from xml.etree import ElementTree
+
 import requests
+
+Episode = namedtuple('Episode', 'title, link, date, id')
+episodes = {}
+
 
 def download_info():
     url = 'https://talkpython.fm/episodes/rss'
@@ -6,8 +13,17 @@ def download_info():
     resp = requests.get(url)
     resp.raise_for_status()
 
-    test = resp.text
-    print(test)
+    dom = ElementTree.fromstring(resp.text)
 
-    return None
+    items = dom.findall('channel/item')
 
+    for idx, item in enumerate(items, start=0):
+        e = Episode(
+            item.find('title').text,
+            item.find('link').text,
+            item.find('pubDate').text,
+            idx
+        )
+
+        episodes[e.id] = e
+    print(episodes)
